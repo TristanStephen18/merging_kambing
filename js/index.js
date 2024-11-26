dd23202f2768c46dac873b4d3b97201eb70efdf7
unknown <trasco_21ur1406@psu.edu.ph>
2024-11-26 22:44:31 +0800

    added a prompt in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 document.getElementById('hello').addEventListener('click', ()=>{
     alert('Hello Maderfaker');
+    let displayer = prompt('Enter your name: ');
+    alert(`Your name is: ${displayer}`);
 });
